d0f15eb156421c068b995803b736385f81ebd356
raymondtieu <raymondtieu@users.noreply.github.com>
2015-12-09 11:39:24 -0500

    Create textArea.js

---
+++ textArea.js
@@ -0,0 +1,55 @@
+/* 
+ * Titanium TextArea with hintText property for iOS 
+ */
+function createTextArea(args) {
+	var data = args || {};
+	
+	var textArea = Ti.UI.createTextArea();
+	
+	setProperties(textArea, data);
+	
+	if (data.hintText !== undefined && data.hintText.trim() !== "") {
+		var hintText = Ti.UI.createLabel({
+			color: "c7c7cd",
+			top: 8,
+			left: 5,
+			text: data.hintText,
+			font: data.font,
+			
+			bubbleParent: true,
+			touchEnabled: false
+		});
+		
+		textArea.add(hintText);
+		
+		textArea.addEventListener("change", function() {
+			if (this.value === "") {
+				hintText.show();
+			} else {
+				hintText.hide();
+			}
+		});
+		
+		if (textArea.value !== "") {
+			hintText.hide();
+		}
+	}
+	
+	textArea.setValue = function(value) {
+		if (value !== "") {
+			textArea.value = value;
+			hintText.hide();
+		} else {
+			hintText.show();
+		}
+	};
+	
+	return textArea;
+}
+
+function setProperties(view, options) {
+	for (var p in options)
+		view[p] = options[p];
+}
+
+exports.createTextArea = createTextArea;
